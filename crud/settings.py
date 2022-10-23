f10082fa4cb58da8693dc3a7e4933d341b057356
Muhammad Shahzaib Afzal <shahzaibafzalofficial@gmail.com>
2022-10-23 10:35:36 +0500

    ninth commit

--- crud/settings.py
+++ crud/settings.py
@@ -25,9 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7!yjbqywb6$&l07csw(o(31f%qez6d#3&(t%s!=6kkexc_2%%%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['.herokuapp.com']
+
 
 
 # Application definition
